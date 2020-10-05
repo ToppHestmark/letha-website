cc2e7fd7c2cab9244cf6b25bef3d76c7e49d50ab
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-10-05 16:51:13 +0200

    navbar

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.nav-links li');
+const main = document.querySelector('.main');
+
 
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('open');
   links.forEach(links => {
     links.classList.toggle('fade')
   })
+  main.classList.toggle('remove')
 })
+
