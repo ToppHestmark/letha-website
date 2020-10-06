ce06698f6a59f814a3180b506dc94d28b46c3203
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-10-06 10:25:36 +0200

    more responsive and changed color on navbar

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.nav-links li');
 const main = document.querySelector('.main');
+const footer = document.querySelector('.footer');
+const navLogo = document.querySelector('.nav-logo');
 
 
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('open');
   links.forEach(links => {
     links.classList.toggle('fade')
-  })
-  main.classList.toggle('remove')
-})
+  });
+
+  main.classList.toggle('remove');
+  footer.classList.toggle('remove');
+  navLogo.classList.toggle('remove');
+});
 
