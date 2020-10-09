40fefdc5a2a367bbe72a732515c47906f6ea449c
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-10-09 09:34:20 +0200

    added subscribe button

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const links = document.querySelectorAll('.nav-links li');
 const main = document.querySelector('.main');
 const footer = document.querySelector('.footer');
 const navLogo = document.querySelector('.nav-logo');
-const shoppingBacIconMobile = document.querySelector('.shoppingBagIconMobile')
+const shoppingBacIconMobile = document.querySelector('.shoppingBagIconMobile');
+const subscription = document.querySelector('.subscribe__formContainer');
 
 
 hamburger.addEventListener('click', () => {
@@ -17,5 +18,6 @@ hamburger.addEventListener('click', () => {
   footer.classList.toggle('remove');
   navLogo.classList.toggle('remove');
   shoppingBacIconMobile.classList.toggle('remove');
+  subscription.classList.toggle('remove');
 });
 
